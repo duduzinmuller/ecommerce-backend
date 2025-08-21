6b53f897e3c86b4a416f2b0ffec3b8d3d1865e06
duduzinmuller <eduardomuller989@gmail.com>
2025-08-21 10:40:59 -0400

    feat(user): integrate GetUserById functionality into user routes

--- src/factiories/user.ts
+++ src/factiories/user.ts
@@ -1,9 +1,12 @@
 import { IdGenerator } from "../adapters/id-generator";
 import { PasswordHasher } from "../adapters/password-hasher";
 import { CreateUserController } from "../controllers/users/create-user";
+import { GetUserByIdController } from "../controllers/users/get-user-by-id";
 import { CreateUserRepository } from "../repositories/users/create-user";
 import { GetUserByEmailRepository } from "../repositories/users/get-user-by-email";
+import { GetUserByIdRepository } from "../repositories/users/get-user-by-id";
 import { CreateUserUseCase } from "../use-cases/users/create-user";
+import { GetUserByIdUseCase } from "../use-cases/users/get-user-by-id";
 
 export const MakeCreateUserController = () => {
   const createUserRepository = new CreateUserRepository();
@@ -18,4 +21,12 @@ export const MakeCreateUserController = () => {
   );
   const createUserController = new CreateUserController(createUserUseCase);
   return createUserController;
+};
+
+export const MakeGetUserByIdController = () => {
+  const getUserByIdRepository = new GetUserByIdRepository();
+  const getUserByIdUseCase = new GetUserByIdUseCase(getUserByIdRepository);
+  const getUserByIdController = new GetUserByIdController(getUserByIdUseCase);
+
+  return getUserByIdController;
 };
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,10 +1,19 @@
 import { Request, Response, Router } from "express";
-import { MakeCreateUserController } from "../factiories/user";
+import {
+  MakeCreateUserController,
+  MakeGetUserByIdController,
+} from "../factiories/user";
 
 export const userRouter = Router();
 
 userRouter.post("/create", async (request: Request, response: Response) => {
   const createdUser = MakeCreateUserController();
   const { statusCode, body } = await createdUser.execute(request);
+  response.status(statusCode).send(body);
+});
+
+userRouter.get("/:userId", async (request: Request, response: Response) => {
+  const getUser = MakeGetUserByIdController();
+  const { statusCode, body } = await getUser.execute(request);
   response.status(statusCode).send(body);
 });
