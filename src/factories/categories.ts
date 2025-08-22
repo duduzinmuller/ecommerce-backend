ff2dd7f1eecd7a7a68983178662d16a6b589e9f5
duduzinmuller <eduardomuller989@gmail.com>
2025-08-21 22:38:18 -0400

    feat(category): integrate GetCategoryBySlug functionality into routes

--- src/factories/categories.ts
+++ src/factories/categories.ts
@@ -1,8 +1,10 @@
 import { IdGeneratorAdapter } from "../adapters/id-generator";
 import { CreateCategoryController } from "../controllers/categories/create-category";
+import { GetCategoryBySlugController } from "../controllers/categories/get-category-by-slug";
 import { CreateCategoryRepository } from "../repositories/categories/create-category";
 import { GetCategoryBySlugRepository } from "../repositories/categories/get-category-by-slug";
 import { CreateCategoryUseCase } from "../use-cases/categories/create-category";
+import { GetCategoryBySlugUseCase } from "../use-cases/categories/get-category-by-slug";
 
 export const MakeCreateCategoryController = () => {
   const createCategoryRepository = new CreateCategoryRepository();
@@ -17,4 +19,15 @@ export const MakeCreateCategoryController = () => {
     createCategoryUseCase,
   );
   return createCategoryController;
+};
+
+export const MakeGetCategoryBySlugController = () => {
+  const getCategoryBySlugRepository = new GetCategoryBySlugRepository();
+  const getCategoryBySlugUseCase = new GetCategoryBySlugUseCase(
+    getCategoryBySlugRepository,
+  );
+  const getCategoryBySlugController = new GetCategoryBySlugController(
+    getCategoryBySlugUseCase,
+  );
+  return getCategoryBySlugController;
 };
--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,10 +1,19 @@
 import { Router, Request, Response } from "express";
-import { MakeCreateCategoryController } from "../factories/categories";
+import {
+  MakeCreateCategoryController,
+  MakeGetCategoryBySlugController,
+} from "../factories/categories";
 
 export const categoryRouter = Router();
 
 categoryRouter.post("/create", async (request: Request, response: Response) => {
   const createdCategory = MakeCreateCategoryController();
   const { statusCode, body } = await createdCategory.execute(request);
+  response.status(statusCode).send(body);
+});
+
+categoryRouter.get("/:slug", async (request: Request, response: Response) => {
+  const getCategoryBySlug = MakeGetCategoryBySlugController();
+  const { statusCode, body } = await getCategoryBySlug.execute(request);
   response.status(statusCode).send(body);
 });
