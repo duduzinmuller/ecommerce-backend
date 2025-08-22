4747cd621ea90e16c71f6be6b39532e8c73c25bf
duduzinmuller <eduardomuller989@gmail.com>
2025-08-21 22:50:48 -0400

    feat(category): add DeleteCategoryController

---
+++ src/controllers/categories/delete-category.ts
@@ -0,0 +1,30 @@
+import { HttpRequest } from "../../types/httpRequest";
+import { DeleteCategoryUseCase } from "../../use-cases/categories/delete-category";
+import { notFound, ok, serverError } from "../helpers/http";
+import { invalidSlugResponse } from "../helpers/validation";
+
+export class DeleteCategoryController {
+  constructor(private deleteCategoryUseCase: DeleteCategoryUseCase) {
+    this.deleteCategoryUseCase = deleteCategoryUseCase;
+  }
+  async execute(httpRequest: HttpRequest) {
+    try {
+      const slug = httpRequest.params?.userId;
+
+      if (!slug) {
+        return invalidSlugResponse();
+      }
+
+      const deletedCategory = await this.deleteCategoryUseCase.execute(slug);
+
+      if (!deletedCategory) {
+        return notFound("Categoria não encontrada");
+      }
+
+      return ok(deletedCategory);
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
+  }
+}
