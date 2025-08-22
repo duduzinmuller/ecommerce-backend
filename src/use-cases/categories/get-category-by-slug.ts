69b5d1e8565a459dbc498a0cffeb5745c2d67a01
duduzinmuller <eduardomuller989@gmail.com>
2025-08-21 22:22:00 -0400

    feat(category): add GetCategoryBySlugUseCase

---
+++ src/use-cases/categories/get-category-by-slug.ts
@@ -0,0 +1,14 @@
+import { GetCategoryBySlugRepository } from "../../repositories/categories/get-category-by-slug";
+
+export class GetCategoryBySlugUseCase {
+  constructor(
+    private getCategoryBySlugRepository: GetCategoryBySlugRepository,
+  ) {
+    this.getCategoryBySlugRepository = getCategoryBySlugRepository;
+  }
+  async execute(slug: string) {
+    const category = await this.getCategoryBySlugRepository.execute(slug);
+
+    return category;
+  }
+}
