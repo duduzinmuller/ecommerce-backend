fcc71a52f6287afa03906ea2b18c4dd3389a7f5c
duduzinmuller <eduardomuller989@gmail.com>
2025-08-21 22:04:58 -0400

    feat(category): validate slug uniqueness in CreateCategoryUseCase

--- src/use-cases/categories/create-category.ts
+++ src/use-cases/categories/create-category.ts
@@ -1,16 +1,27 @@
 import { IdGeneratorAdapter } from "../../adapters/id-generator";
+import { SlugAlreadyInCreateError } from "../../error/category";
 import { CreateCategoryRepository } from "../../repositories/categories/create-category";
+import { GetCategoryBySlugRepository } from "../../repositories/categories/get-category-by-slug";
 import { Category } from "../../types/category";
 
 export class CreateCategoryUseCase {
   constructor(
     private idGeneratorAdapter: IdGeneratorAdapter,
     private createCategoryRepository: CreateCategoryRepository,
+    private getCategoryBySlugRepository: GetCategoryBySlugRepository,
   ) {
     this.idGeneratorAdapter = idGeneratorAdapter;
     this.createCategoryRepository = createCategoryRepository;
+    this.getCategoryBySlugRepository = getCategoryBySlugRepository;
   }
   async execute(createCategoryParams: Category) {
+    const withExistsSlug = await this.getCategoryBySlugRepository.execute(
+      createCategoryParams.slug,
+    );
+
+    if (withExistsSlug) {
+      throw new SlugAlreadyInCreateError(createCategoryParams.slug);
+    }
     const categoryId = await this.idGeneratorAdapter.execute();
 
     const { id, ...restParams } = createCategoryParams;
