b9972355b0a527cb2b11566161661bb4f68678cb
duduzinmuller <eduardomuller989@gmail.com>
2025-08-24 11:58:23 -0400

    feat(product): add CreateProductUseCase

---
+++ src/use-cases/products/create-product.ts
@@ -0,0 +1,37 @@
+import { IdGeneratorAdapter } from "../../adapters/id-generator";
+import { SlugAlreadyInCreateError } from "../../error/category";
+import { Product } from "../../interfaces/product";
+import { CreateProductRepository } from "../../repositories/products/create-product";
+import { GetProductBySlugRepository } from "../../repositories/products/get-product-by-slug";
+
+export class CreateProductUseCase {
+  constructor(
+    private createProductRepository: CreateProductRepository,
+    private getProductBySlugRepository: GetProductBySlugRepository,
+    private idGeneratorAdapter: IdGeneratorAdapter,
+  ) {
+    this.createProductRepository = createProductRepository;
+    ((this.getProductBySlugRepository = getProductBySlugRepository),
+      (this.idGeneratorAdapter = idGeneratorAdapter));
+  }
+  async execute(createProductParams: Product) {
+    const slugProductIsExists = await this.getProductBySlugRepository.execute(
+      createProductParams.slug,
+    );
+    if (slugProductIsExists) {
+      throw new SlugAlreadyInCreateError(createProductParams.slug);
+    }
+    const productId = await this.idGeneratorAdapter.execute();
+
+    const { id, ...restParams } = createProductParams;
+
+    const product = {
+      id: productId,
+      ...restParams,
+    };
+
+    const createdProduct = await this.createProductRepository.execute(product);
+
+    return createdProduct;
+  }
+}
