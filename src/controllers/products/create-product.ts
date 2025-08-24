e1797e7eb657e760995d06a44dc08f897d6ba4bb
duduzinmuller <eduardomuller989@gmail.com>
2025-08-24 12:30:58 -0400

    feat(product): add CreateProductController

---
+++ src/controllers/products/create-product.ts
@@ -0,0 +1,32 @@
+import { ZodError } from "zod";
+import { HttpRequest } from "../../interfaces/httpRequest";
+import { CreateProductUseCase } from "../../use-cases/products/create-product";
+import { badRequest, created, serverError } from "../helpers/http";
+import { SlugAlreadyInCreateError } from "../../error/category";
+import { createProductSchema } from "../../schema/product";
+
+export class CreateProductController {
+  constructor(private createProductUseCase: CreateProductUseCase) {
+    this.createProductUseCase = createProductUseCase;
+  }
+  async execute(httpRequest: HttpRequest) {
+    try {
+      const params = httpRequest.body;
+
+      await createProductSchema.parse(params);
+
+      const createdProduct = await this.createProductUseCase.execute(params);
+
+      return created(createdProduct);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof ZodError) {
+        return badRequest(error.issues[0]?.message);
+      }
+      if (error instanceof SlugAlreadyInCreateError) {
+        return badRequest(error.message);
+      }
+      return serverError();
+    }
+  }
+}
---
+++ src/schema/product.ts
@@ -0,0 +1,31 @@
+import z from "zod";
+
+export const createProductSchema = z.object({
+  name: z.string().trim().min(1, {
+    message: "O nome do produto é obrigatório",
+  }),
+  slug: z
+    .string()
+    .trim()
+    .min(1, {
+      message: "O slug é obrigatório.",
+    })
+    .transform((val) => val.toLowerCase().replace(/\s+/g, "-")),
+  description: z.string().trim().min(30, {
+    message: "A descrição deve ter pelo menos 30 caracteres",
+  }),
+  price: z
+    .string()
+    .min(1, "O preço é obrigatório")
+    .regex(
+      /^\d+(\.\d{1,2})?$/,
+      "O preço deve ser um número decimal válido (ex: 10.50)",
+    )
+    .transform((val) => parseFloat(val)),
+  stock: z
+    .number()
+    .int()
+    .min(0, "O estoque deve ser um número inteiro não negativo"),
+  category_id: z.string().trim().min(1, "A categoria é obrigatória"),
+  image_url: z.string().url("A URL da imagem deve ser válida").optional(),
+});
