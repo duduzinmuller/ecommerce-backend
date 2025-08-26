c6491d1bf86ec5d4b87332d76fabc95791f6b507
duduzinmuller <eduardomuller989@gmail.com>
2025-08-25 20:14:37 -0400

    feat(product): ensure UpdateProductUseCase to validate category existence

--- src/use-cases/products/update-product.ts
+++ src/use-cases/products/update-product.ts
@@ -1,14 +1,20 @@
-import { SlugAlreadyInCreateError } from "../../error/category";
+import {
+  CategoryIsNotFound,
+  SlugAlreadyInCreateError,
+} from "../../error/category";
 import { UpdateProductParams } from "../../interfaces/product";
+import { GetCategoryByIdRepository } from "../../repositories/categories/get-category-by-id";
 import { GetProductBySlugRepository } from "../../repositories/products/get-product-by-slug";
 import { UpdateProductRepository } from "../../repositories/products/update-product";
 
 export class UpdateProductUseCase {
   constructor(
     private getProductBySlugRepository: GetProductBySlugRepository,
+    private getCategoryByIdRepository: GetCategoryByIdRepository,
     private updateProductRepository: UpdateProductRepository,
   ) {
     this.getProductBySlugRepository = getProductBySlugRepository;
+    this.getCategoryByIdRepository = getCategoryByIdRepository;
     this.updateProductRepository = updateProductRepository;
   }
   async execute(slug: string, updateProductParams: UpdateProductParams) {
@@ -20,6 +26,14 @@ export class UpdateProductUseCase {
       if (slugProductIsExists && slugProductIsExists.slug !== slug) {
         throw new SlugAlreadyInCreateError(updateProductParams.slug);
       }
+    }
+
+    const category = await this.getCategoryByIdRepository.execute(
+      updateProductParams.category_id!,
+    );
+
+    if (!category) {
+      throw new CategoryIsNotFound();
     }
 
     const product = { ...updateProductParams };
