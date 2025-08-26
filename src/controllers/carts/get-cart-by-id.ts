2cd0f7760b1f3a42561cdf44fb1cd90d6570f075
duduzinmuller <eduardomuller989@gmail.com>
2025-08-26 17:36:45 -0400

    feat(cart): enhance GetCartByIdController to include user permission validation

--- src/controllers/carts/get-cart-by-id.ts
+++ src/controllers/carts/get-cart-by-id.ts
@@ -1,24 +1,29 @@
 import { HttpRequest } from "../../interfaces/httpRequest";
 import { GetCartByIdUseCase } from "../../use-cases/carts/get-cart-by-id";
-import { notFound, ok, serverError } from "../helpers/http";
+import { notFound, ok, serverError, forbidden } from "../helpers/http";
 import { requiredCartIdResponse } from "../helpers/validation";
 
 export class GetCartByIdController {
-  constructor(private getCartByIdUseCase: GetCartByIdUseCase) {
-    this.getCartByIdUseCase = getCartByIdUseCase;
+  constructor(private getCartByIdRepository: GetCartByIdUseCase) {
+    this.getCartByIdRepository = getCartByIdRepository;
   }
   async execute(httpRequest: HttpRequest) {
     try {
       const cartId = httpRequest.params?.cartId;
+      const userId = httpRequest.userId;
 
       if (!cartId) {
         return requiredCartIdResponse();
       }
 
-      const getCartById = await this.getCartByIdUseCase.execute(cartId);
+      const getCartById = await this.getCartByIdRepository.execute(cartId);
 
       if (!getCartById) {
         return notFound("Carrinho não encontrado");
+      }
+
+      if (getCartById.user_id !== userId) {
+        return forbidden("Você não tem permissão para acessar este carrinho");
       }
 
       return ok(getCartById);
