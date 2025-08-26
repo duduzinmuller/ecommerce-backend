788e9a56b8a836ae184d81b54712735ee7fec96a
duduzinmuller <eduardomuller989@gmail.com>
2025-08-26 17:06:11 -0400

    feat(cart): add GetCartByIdRepository

---
+++ src/repositories/carts/get-cart-by-id.ts
@@ -0,0 +1,24 @@
+import { eq } from "drizzle-orm";
+import { db } from "../../db";
+import { cart_items, carts, products } from "../../db/schema";
+
+export class GetCartByIdRepository {
+  async execute(cartId: string) {
+    const [cart] = await db.select().from(carts).where(eq(carts.id, cartId));
+
+    const items = await db
+      .select({
+        itemId: cart_items.id,
+        quantity: cart_items.quantity,
+        productId: products.id,
+        name: products.name,
+        price: products.price,
+        imageUrl: products.image_url,
+      })
+      .from(cart_items)
+      .innerJoin(products, eq(cart_items.product_id, products.id))
+      .where(eq(cart_items.cart_id, cartId));
+
+    return { ...cart, items };
+  }
+}
