cc5b5b3a76327707f54bbbbe6848793418f084a6
duduzinmuller <eduardomuller989@gmail.com>
2025-08-25 21:30:30 -0400

    feat(dependencies): add chalk for enhanced console logging

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import "dotenv/config";
 import { app } from "./app";
+import chalk from "chalk";
 
-app.listen(8000, () => console.log("Rodando na porta 8000"));
+app.listen(process.env.PORT, () => {
+  console.log(
+    chalk.magentaBright.bold("🚀 SERVER ") +
+      chalk.cyanBright.bold("IS RUNNING ") +
+      chalk.yellowBright.bold(`ON PORT ${process.env.PORT}`),
+  );
+});
