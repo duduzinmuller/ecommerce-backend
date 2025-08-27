8983fe23dd37a653c7661f53bac6dc4956cff725
duduzinmuller <eduardomuller989@gmail.com>
2025-08-27 11:35:03 -0400

    feat(order): enhance CreateOrderUseCase to validate cart existence and items

--- src/use-cases/orders/create-order.ts
+++ src/use-cases/orders/create-order.ts
@@ -1,6 +1,9 @@
 import { IdGeneratorAdapter } from "../../adapters/id-generator";
+import { EmptyCartError } from "../../error/cart-item";
+import { CategoryIsNotFound } from "../../error/category";
 import { UserNotFoundError } from "../../error/user";
 import { Order } from "../../interfaces/order";
+import { GetCartByUserIdRepository } from "../../repositories/carts/get-cart-by-user-id";
 import { CreateOrderRepository } from "../../repositories/orders/create-order";
 import { GetUserByIdRepository } from "../../repositories/users/get-user-by-id";
 
@@ -9,10 +12,12 @@ export class CreateOrderUseCase {
     private getUserByIdRepository: GetUserByIdRepository,
     private idGeneratorAdapter: IdGeneratorAdapter,
     private createOrderRepository: CreateOrderRepository,
+    private getCartByUserIdRepository: GetCartByUserIdRepository,
   ) {
     this.getUserByIdRepository = getUserByIdRepository;
     this.idGeneratorAdapter = idGeneratorAdapter;
     this.createOrderRepository = createOrderRepository;
+    this.getCartByUserIdRepository = getCartByUserIdRepository;
   }
   async execute(createOrderParams: Order) {
     const orderId = await this.idGeneratorAdapter.execute();
@@ -23,6 +28,18 @@ export class CreateOrderUseCase {
 
     if (!userId) {
       throw new UserNotFoundError("Usuario não encontrado");
+    }
+
+    const cart = await this.getCartByUserIdRepository.execute(
+      createOrderParams.user_id,
+    );
+
+    if (!cart) {
+      throw new CategoryIsNotFound();
+    }
+
+    if (cart.items.length === 0) {
+      throw new EmptyCartError();
     }
 
     const { id, ...restParams } = createOrderParams;
