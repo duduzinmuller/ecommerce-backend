ead6c53d5ccfefff96a3264b7fc410e562a3b2b5
duduzinmuller <eduardomuller989@gmail.com>
2025-08-26 21:35:32 -0400

    feat(cart_items): add RemoveCartItemRepository

---
+++ src/repositories/cart-items/remove-cart-item.ts
@@ -0,0 +1,14 @@
+import { eq } from "drizzle-orm";
+import { db } from "../../db";
+import { cart_items } from "../../db/schema";
+
+export class RemoveCartItemRepository {
+  async execute(itemId: string) {
+    const [deletedItem] = await db
+      .delete(cart_items)
+      .where(eq(cart_items.id, itemId))
+      .returning();
+
+    return !!deletedItem;
+  }
+}
