a3270b0e0f0075f420138c1c93dcffa4ca3e1140
duduzinmuller <eduardomuller989@gmail.com>
2025-08-28 08:02:19 -0400

    refactor(order): update CreateOrderRepository to return inserted order directly

--- src/repositories/orders/create-order.ts
+++ src/repositories/orders/create-order.ts
@@ -4,21 +4,24 @@ import { Order } from "../../interfaces/order";
 
 export class CreateOrderRepository {
   async execute(createOrderParams: Order) {
-    const order = await db.insert(orders).values({
-      id: createOrderParams.id,
-      user_id: createOrderParams.user_id,
-      order_date: createOrderParams.order_date,
-      status: createOrderParams.status,
-      total: createOrderParams.total,
-      delivery_street: createOrderParams.delivery_street,
-      delivery_number: createOrderParams.delivery_number,
-      delivery_neighborhood: createOrderParams.delivery_neighborhood,
-      delivery_complement: createOrderParams.delivery_complement,
-      delivery_city: createOrderParams.delivery_city,
-      delivery_state: createOrderParams.delivery_state,
-      delivery_zip_code: createOrderParams.delivery_zip_code,
-      document: createOrderParams.document,
-    });
+    const [order] = await db
+      .insert(orders)
+      .values({
+        id: createOrderParams.id,
+        user_id: createOrderParams.user_id,
+        order_date: createOrderParams.order_date,
+        status: createOrderParams.status,
+        total: createOrderParams.total,
+        delivery_street: createOrderParams.delivery_street,
+        delivery_number: createOrderParams.delivery_number,
+        delivery_neighborhood: createOrderParams.delivery_neighborhood,
+        delivery_complement: createOrderParams.delivery_complement,
+        delivery_city: createOrderParams.delivery_city,
+        delivery_state: createOrderParams.delivery_state,
+        delivery_zip_code: createOrderParams.delivery_zip_code,
+        document: createOrderParams.document,
+      })
+      .returning();
 
     return order;
   }
