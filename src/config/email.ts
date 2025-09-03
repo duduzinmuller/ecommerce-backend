3b39c709d9e47e2a0ad7957ca2a52c8c471d5418
duduzinmuller <eduardomuller989@gmail.com>
2025-09-03 15:56:25 -0400

    feat(config): add nodemailer config

---
+++ src/config/email.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
