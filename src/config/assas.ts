34ae061f31649bbb9cd2b95eb9823e3f896ff55b
duduzinmuller <eduardomuller989@gmail.com>
2025-09-05 15:40:54 -0400

    fix(asaas): enable error printing and disable sandbox mode in AsaasClient configuration

--- src/config/assas.ts
+++ src/config/assas.ts
@@ -1,6 +1,6 @@
 import { AsaasClient } from "asaas";
 
 export const asaas = new AsaasClient(process.env.ASAAS_API_KEY!, {
-  sandboxUrl: "https://sandbox.asaas.com/api/v3",
-  printError: false,
+  printError: true,
+  sandbox: false,
 });
