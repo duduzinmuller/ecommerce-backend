c789497f3aa39a3eb28c7903323c62b7d9744167
duduzinmuller <eduardomuller989@gmail.com>
2025-09-05 10:14:59 -0400

    feat(payment): add CreatePaymentController

---
+++ src/controllers/payment/create-payment.ts
@@ -0,0 +1,32 @@
+import { ZodError } from "zod";
+import { HttpRequest } from "../../interfaces/httpRequest";
+import { PaymentBaseSchema } from "../../schema/payment";
+import { CreatePaymentUseCase } from "../../use-cases/payment/create-payment";
+import { badRequest, created, serverError } from "../helpers/http";
+import { MethodPaymentNotError } from "../../error/payment";
+
+export class CreatePaymentController {
+  constructor(private createPaymentUseCase: CreatePaymentUseCase) {
+    this.createPaymentUseCase = createPaymentUseCase;
+  }
+  async execute(httpRequest: HttpRequest) {
+    try {
+      const params = httpRequest.body;
+
+      await PaymentBaseSchema.parse(params);
+
+      const payment = await this.createPaymentUseCase.execute(params);
+
+      return created(payment);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return badRequest(error.issues[0]?.message);
+      }
+      if (error instanceof MethodPaymentNotError) {
+        return badRequest(error.message);
+      }
+      console.error(error);
+      return serverError();
+    }
+  }
+}
