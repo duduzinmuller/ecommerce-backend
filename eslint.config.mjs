4295d113a59dab001d7334c0d58d28e28d371b25
duduzinmuller <eduardomuller989@gmail.com>
2025-09-06 19:20:02 -0400

    chore; ensure prettier unchanged setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,10 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
 ]);
